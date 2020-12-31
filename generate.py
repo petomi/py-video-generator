d60e07cda4c77f280c3c9442f0ecd577c82cdc14
Michael Peto <michael_peto@worldvision.ca>
2020-12-31 18:36:52 -0500

    multithreading working - next step should be stopping pool execution on error and logging of pool results should work

--- generate.py
+++ generate.py
@@ -5,32 +5,18 @@
 # Then run `pip install -r requirements.txt`
 # Uses either command line arguments or variables defined in config.yml file for configuration.
 
-from multiprocessing import Process, ProcessError
+import multiprocessing as mp
 from subprocess import run
 import argparse
+import sys
 import yaml
-
-# FUNCTION FOR CALLING COLLECT, COMBINE, AND UPLOAD AS SUBPROCESSES
-def generate_video_using_id(id, api_url_pattern):
-    print(f'Generating video for id: {id}')
-    url = api_url_pattern.replace('{UNIQUE_ID}', id)
-    # call collect.py to get video clips
-    print(f'Collecting media for id: {id}')
-    run(['py', 'collect.py', '--api_url', url, '--unique_clip_id', id])
-    # call combine.py to combine and render the clips
-    print(f'Rendering video for id: {id}')
-    run(['py', 'combine.py', '--unique_clip_id', id])
-    # call upload.py to upload the finished clips
-    print(f'Uploading finished video for id: {id}')
-    run(['py', 'upload.py', '--unique_clip_id', id])
-
-
-# MAIN PROGRAM
 
 # define optional command line arguments using argparse
 parser = argparse.ArgumentParser(description='GENERATE.PY //// Generate videos based on cloud-hosted media accessible via API and upload the result to Azure.')
 parser.add_argument('--unique_ids_file', nargs='?', help='the file containing a list of unique ids to generate videos for, each on a new line')
 parser.add_argument('--api_url_pattern', nargs='?', help='the template for the media API we pull media from, which will have the unique ids interpolated into it. Use `{UNIQUE_ID}` as placeholder.')
+parser.add_argument('--log_file', nargs='?', help='the location of the log file.')
+parser.add_argument('--error_log_file', nargs='?', help='the location of the error log file.')
 args = parser.parse_args()
 
 # get YAML config.yml file settings, accessed using cfg[key][subkey]
@@ -40,28 +26,69 @@ cfg = yaml.load(file, Loader= yaml.FullLoader)
 # set up variables (command line overrides config.yml)
 unique_ids_file = args.unique_ids_file or cfg['generate']['unique_ids_file']
 api_url_pattern = args.api_url_pattern or cfg['generate']['api_url_pattern']
+log_file = args.log_file or cfg['generate']['log_file']
+error_log_file = args.error_log_file or cfg['generate']['error_log_file']
 
-# read list of ids from file
-print(f'Importing ids from: {unique_ids_file}')
-with open(unique_ids_file, 'r') as f:
-    id_list = f.read().splitlines() # used instead of readlines to remove \n characters
+# FUNCTION FOR CALLING COLLECT, COMBINE, AND UPLOAD AS SUBPROCESSES
+def generate_video_using_id(id, api_url_pattern):
+    try:
+        print(f'Generating video for id: {id}')
+        url = api_url_pattern.replace('{UNIQUE_ID}', id)
+        # call collect.py to get video clips
+        collect = run(['py', 'collect.py', '--api_url', url, '--unique_clip_id', id], capture_output=True)
+        if len(collect.stderr) > 0:
+            raise Exception(f'Subprocess failed. Stopping execution. {str(collect.stderr)}')
+        # call combine.py to combine and render the clips
+        combine = run(['py', 'combine.py', '--unique_clip_id', id], capture_output=True)
+        if len(combine.stderr) > 0:
+            raise Exception(
+                f'Subprocess failed. Stopping execution. {str(combine.stderr)}')
+        # call upload.py to upload the finished clips
+        upload = run(['py', 'upload.py', '--unique_clip_id', id], capture_output=True)
+        if len(upload.stderr) > 0:
+            raise Exception(
+                f'Subprocess failed. Stopping execution. {str(upload.stderr)}')
+    except Exception as ex:
+        print(f'Error generating video for id {id}:')
+        print(ex)
 
-try:
-    open_processes=[]
-    # for each unique ID listed in the file specified in config, begin generating and uploading video as threaded process
-    for id in id_list:
-        p = Process(target=generate_video_using_id, args=(id, api_url_pattern)).start()
-        open_processes.append(p)
+# LOG RESULTS
+def log_results(result):
+    with open(log_file, 'a+') as l:
+        l.write('\n' + str(result))
 
-    for process in open_processes:
-        process.join()
+# LOG FAILED RESULTS
+def log_failed_results(result):
+    with open(error_log_file, 'a+') as l:
+        l.write('\n' + str(result))
 
-        if process.exception:
-            error, traceback = process.exception
-            print(traceback)
+# ABORT ALL PROCESSES
+def abort_all_processes(exctype, value, traceback):
+    for p in mp.active_children():
+        print('active child: ' + str(p))
+        p.terminate() # TODO - figure out why this doesn't work
 
-    print('All video clips have been generated.')
+# MAIN PROGRAM
 
-except Exception as ex:
-    print('Error generating videos:')
-    print(ex)
+if __name__ == '__main__':
+    # read list of ids from file
+    print(f'Importing ids from: {unique_ids_file}')
+    with open(unique_ids_file, 'r') as f:
+        id_list = f.read().splitlines() # used instead of readlines to remove \n characters
+
+    try:
+        pool = mp.Pool()
+
+        # for each unique ID listed in the file specified in config, begin generating and uploading video as threaded process
+        for id in id_list:
+            pool.apply_async(generate_video_using_id, args=(id, api_url_pattern), callback=log_results, error_callback=log_failed_results)
+
+        sys.excepthook = abort_all_processes # TODO - figure out how to end all processes properly
+        pool.close()
+        pool.join()
+
+        print('All video clips have been generated.')
+
+    except Exception as ex:
+        print('Error generating videos:')
+        print(ex)
